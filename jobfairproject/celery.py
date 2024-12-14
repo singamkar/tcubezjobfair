d4ed135a0017ee5fae0b6f38307ed291a1c0e0eb
singamkar <162546787+singamkar@users.noreply.github.com>
2024-12-14 14:18:11 +0530

    Add files via upload

---
+++ jobfairproject/celery.py
@@ -0,0 +1,18 @@
+# celery.py
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobfairproject.settings')
+
+app = Celery('jobfairproject')
+
+# Load task modules from all registered Django app configs
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
